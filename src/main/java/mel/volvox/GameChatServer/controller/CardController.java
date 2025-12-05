232bac329fd000ad69681aca8ba6e20f721dfa36
Mel Nicholson <mhnicholson@yahoo.com>
2025-12-04 18:28:44 -0800

    implement play-again and quit

--- src/main/java/mel/volvox/GameChatServer/controller/CardController.java
+++ src/main/java/mel/volvox/GameChatServer/controller/CardController.java
@@ -34,16 +34,7 @@ public class CardController {
     @PutMapping("cards/new/{game}")
     @ResponseBody
     public Tableau createGame(@PathVariable String game) {
-        Class<? extends CardGame> clazz = name2class.get(game);
-        if(clazz==null) throw new IllegalStateException("Unknown game type "+game);
-        try {
-            CardGame cg = clazz.getConstructor().newInstance();
-            cg.init();
-            id2game.put(cg.getLayout().getId(), cg);
-            return cg.getLayout();
-        } catch (Exception e) {
-            throw new IllegalStateException("Game construction failed: "+e.getClass().getName());
-        }
+        return makeGame(game);
     }
 
     @GetMapping("cards/list")
@@ -65,5 +56,36 @@ public class CardController {
                               @PathVariable int x,
                               @PathVariable int y) {
         return findGame(game).select(place, x, y);
+    }
+
+    @PutMapping("cards/delete/{game}")
+    @ResponseBody
+    public Tableau deleteGame(@PathVariable String game) {
+        CardGame t = id2game.remove(game);
+        //TODO add resignation to stats
+        if(t == null) throw new IllegalStateException("No such game");
+        else return t.getLayout();
+    }
+
+    @PutMapping("cards/change/{before}/{after}")
+    @ResponseBody
+    public Tableau startAnother(@PathVariable String before,
+                                @PathVariable String after) {
+        CardGame cg = id2game.remove(before);
+        //TODO add resignation to stats
+        return makeGame(after);
+    }
+
+    private Tableau makeGame(String game) {
+        Class<? extends CardGame> clazz = name2class.get(game);
+        if(clazz==null) throw new IllegalStateException("Unknown game type "+game);
+        try {
+            CardGame cg = clazz.getConstructor().newInstance();
+            cg.init();
+            id2game.put(cg.getLayout().getId(), cg);
+            return cg.getLayout();
+        } catch (Exception e) {
+            throw new IllegalStateException("Game construction failed: "+e.getClass().getName());
+        }
     }
 }
